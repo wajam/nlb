662355ad7aa5ca6bcd6992da6a2cd29e802b5e1f
Clément GARNIER <clement@wajam.com>
2013-07-18 09:25:59 -0400

    Cleaned dependency resolvers

--- project/Project.scala
+++ project/Project.scala
@@ -18,13 +18,9 @@ object NlbBuild extends Build {
     "Oracle Maven2 Repo" at "http://download.oracle.com/maven",
     "Sonatype" at "http://oss.sonatype.org/content/repositories/release",
     "Cloudera" at "https://repository.cloudera.com/artifactory/cloudera-repos/",
-    "Twitter" at "http://maven.twttr.com/",
     "Scallop" at "http://mvnrepository.com/",
-    "spy" at "http://files.couchbase.com/maven2/",
-    "Twitter" at "http://maven.twttr.com/",
     "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
-    "Spray Repository" at "http://repo.spray.io",
-    "Scallop" at "http://mvnrepository.com/"
+    "Spray Repository" at "http://repo.spray.io"
   )
 
   var commonDeps = Seq(
@@ -40,9 +36,9 @@ object NlbBuild extends Build {
     "junit" % "junit" % "4.11" % "test,it",
     "org.mockito" % "mockito-core" % "1.9.0" % "test,it",
     "com.typesafe" % "config" % "1.0.2",
-    "com.typesafe.akka" % "akka-actor_2.10" % "2.2.0-RC1",
-    "com.typesafe.akka" % "akka-testkit_2.10" % "2.2.0-RC1",
-    "com.typesafe.akka" % "akka-slf4j_2.10" % "2.2.0-RC1" exclude("org.slf4j", "slf4j-api"),
+    "com.typesafe.akka" %% "akka-actor" % "2.2.0-RC1",
+    "com.typesafe.akka" %% "akka-testkit" % "2.2.0-RC1",
+    "com.typesafe.akka" %% "akka-slf4j" % "2.2.0-RC1" exclude("org.slf4j", "slf4j-api"),
     "io.spray" % "spray-io" % "1.2-M8",
     "io.spray" % "spray-http" % "1.2-M8",
     "io.spray" % "spray-util" % "1.2-M8",
