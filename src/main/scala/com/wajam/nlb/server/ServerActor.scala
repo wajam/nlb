1810133f1eceb20d7b87e728bfe7d78176a7a8a0
Clément GARNIER <clement@wajam.com>
2013-07-23 16:52:37 -0400

    Added /nlb_health path for health check

--- src/main/scala/com/wajam/nlb/server/ServerActor.scala
+++ src/main/scala/com/wajam/nlb/server/ServerActor.scala
@@ -6,9 +6,10 @@ import scala.concurrent.duration.Duration
 import spray.can.Http
 import spray.http._
 import spray.util.SprayActorLogging
+import HttpMethods.GET
 import com.yammer.metrics.scala.Instrumented
-import com.wajam.nrv.tracing.{RpcName, Annotation, Tracer}
-import com.wajam.nlb.util.{ResolvingException, TracedRequest, Router}
+import com.wajam.nrv.tracing.Tracer
+import com.wajam.nlb.util.{ResolvingException, Router}
 import com.wajam.nlb.client.SprayConnectionPool
 import com.wajam.nlb.forwarder.ForwarderActor
 
@@ -29,6 +30,9 @@ class ServerActor(pool: SprayConnectionPool, router: Router, forwarderIdleTimeou
     // when a new connection comes in we register ourselves as the connection handler
     case _: Http.Connected =>
       sender ! Http.Register(self)
+
+    case healthCheck @ HttpRequest(GET, Uri.Path("/nlb_health"), _, _, _) =>
+      sender ! HttpResponse()
 
     case request: HttpRequest =>
       val client = sender
