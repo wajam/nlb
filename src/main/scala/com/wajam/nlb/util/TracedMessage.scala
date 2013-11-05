b1e8e5a514b0eb02600515faa2b6f1274b16bbc6
Clément GARNIER <clement@wajam.com>
2013-11-05 09:43:34 -0500

    Use lazy vals inst. of def in TracedRequest

--- src/main/scala/com/wajam/nlb/util/TracedMessage.scala
+++ src/main/scala/com/wajam/nlb/util/TracedMessage.scala
@@ -81,9 +81,9 @@ trait TracedMessageFactory[T <: HttpMessage] {
 
 case class TracedRequest(get: HttpRequest, context: Option[TraceContext], timer: StartStopTimer) extends TracedMessage(get, context, timer) {
 
-  def path: String = get.uri.path.toString
-  def method: String = get.method.toString
-  def address = new InetSocketAddress(get.uri.authority.host.address, get.uri.authority.port)
+  lazy val path: String = get.uri.path.toString
+  lazy val method: String = get.method.toString
+  lazy val address = new InetSocketAddress(get.uri.authority.host.address, get.uri.authority.port)
 
   def withNewContext(context: Option[TraceContext]) = copy(get = get.withHeaders(getNewContextHeaders(context)))
 }
